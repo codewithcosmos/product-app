4cf35611809b715a43811fe9351dc5ddf045879d
Cosmos Mhlanga <codewithcosmos@gmail.com>
2024-06-21 00:32:30 +0200

    Modified, package, order and server

--- models/Order.js
+++ models/Order.js
@@ -1,14 +1,32 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    products: [{
-        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-        quantity: { type: Number, required: true }
-    }],
-    totalAmount: { type: Number, required: true },
-    paymentStatus: { type: String, required: true, enum: ['Pending', 'Completed'] },
-    createdAt: { type: Date, default: Date.now }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  products: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  paymentStatus: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('Order', orderSchema);
+module.exports = mongoose.models.Order || mongoose.model('Order', orderSchema);
